d930f1418b2bb4ea3006b7de54def8c8892328ef
HNNX <nejc.stibilj01@gmail.com>
2020-07-21 14:48:46 +0200

    Resources - Local Scope, Window Scope, App Score

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,18 @@ namespace WPFTuts
     /// </summary>
     public partial class App : Application
     {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow w = new MainWindow();
+
+            if(e.Args.Length == 1)
+            {
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+            }
+
+            w.Show();
+        }
+       
     }
 }
